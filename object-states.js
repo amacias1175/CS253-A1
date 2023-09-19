8aa28f397978513a30b4f5252e1bdff9d1077f62
Angel Macias <amacias@iwu.edu>
2023-09-18 20:07:02 -0500

    documentation done, code complete

--- object-states.js
+++ object-states.js
@@ -1,7 +1,10 @@
 /**
-    (replace this with your function comment which describes 
-    what your function does, what the arguments are, and what 
-    the output should be.)
+    the findvotes function takes an object named states and an array named locs. This function will iterate over the locs array and 
+    split each index to obtain the state and the zip vote and declare them to a variable. From this, we get the votes by indexing
+    the states object from the state as the first index and the zip as the second index to get the votes integer and then we
+    declare that number to a variable. We push the votes and the locs array index into an array. Using that array, we push that array 
+    and the votes number into another array. Finally we reset the votes_array to prevent a circular array object. In the end,
+    we get an array with arrays that constain the state, zip code, and the vote count. 
 */
 function findVotes(states, locs) {
     const votes_array = [];
@@ -26,7 +29,7 @@ function findVotes(states, locs) {
    
 }
 
-  
+//test cases from below this comment
 const election = {
     'ca': {94301: 3456, 94303: 255},
     'tx': {77494: 400, 75034: 9863},
