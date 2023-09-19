7045412723ea7a30ce41921a9049a79065b47b77
Angel Macias <amacias@iwu.edu>
2023-09-18 19:37:58 -0500

    code complete

--- object-states.js
+++ object-states.js
@@ -4,5 +4,33 @@
     the output should be.)
 */
 function findVotes(states, locs) {
-    // your code here
+    const votes_array = [];
+    var result_array = [];
+    var state = "";
+    var zip = "";
+    var votes = 0;
+    
+    for (i = 0; i < locs.length; i++) {
+    	var position = locs[i].split("-");
+      state = position[0];
+      zip = position[1];
+      votes = states[state][zip];
+      
+      result_array.push(locs[i], votes);
+      votes_array.push(result_array);
+      result_array = [];
+     
+    }
+   	return votes_array;
+   
+   
 }
+
+  
+const election = {
+    'ca': {94301: 3456, 94303: 255},
+    'tx': {77494: 400, 75034: 9863},
+    
+}
+
+console.log(findVotes(election, ['ca-94303', 'tx-77494', 'ca-94301', 'tx-75034']));
